5774a7f78e12e75b35a961e6f95508f05bea6430
piaggiosi104-tech <piaggiosi104@gmail.com>
2026-04-16 10:06:14 +0200

    Add files via upload

---
+++ generatore bestemmie creative 0.2.2 .py
@@ -0,0 +1,25 @@
+import random
+gruppo_a = ["dio", "gesù", "giuda", "cristo"]
+gruppo_b = [
+    "Pennarello", "bistecca", "lampadario", "cane", "disonesto", "cane", "smerdarolo", 
+"porco", "boscaiolo", "radioattivo", "coleroso", "troione", "ricchione", "merda",
+ "maialone", "puttaniere", "acquario", "polenta", "cacciavite", "ingordo", "sborrato", 
+ "polacco", "prezzemolo", "cipresso", "prosciutto", "ippopotamo", "elefante", "gelato",
+  "cancellino", "quaderno", "aragosta", "philadelphia", "carota", "porcaccio", "lenzuola",
+   "piatto", "gorilla", "falena", "ciliegia", "canguro"
+]
+print(f"--- GENERATORE PRONTO ---")
+print(f"Caricate {len(gruppo_a)} parole in A e {len(gruppo_b)} in B.")
+print("Premi INVIO per generare, scrivi 'esci' per chiudere.")
+
+while True:
+    comando = input("\n> ").lower().strip()
+    
+    if comando == "esci":
+        break
+    
+    if gruppo_a and gruppo_b:
+        # Sceglie a caso e stampa
+        print(f"{random.choice(gruppo_a)} {random.choice(gruppo_b)}")
+    else:
+        print("Errore: Assicurati di aver riempito entrambi i gruppi!")
